b577b75c371311146cbeb7aaa06b9eb9370e60e7
Joshua Porter <joshporter1@gmail.com>
2016-07-14 05:19:38 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -12,4 +13,5 @@ def update():
 
 # start the server
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
